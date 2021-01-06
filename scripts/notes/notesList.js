792f5a0b52ba38e3356fd3d7cc26c4a2f715b9b6
skgilmore <skg.gilmore@gmail.com>
2021-01-06 13:46:10 -0600

    drop down for suspects working

--- scripts/notes/notesList.js
+++ scripts/notes/notesList.js
@@ -1,5 +1,7 @@
 import { getNotes, useNotes } from "./notesProvider.js";
 import { NoteHTMLConverter } from "./notes.js";
+import { useCriminals } from "../criminals/criminalsProvider.js"
+
 let visible = false
 // Query the DOM for the element that your notes will be added to 
 const contentTarget = document.querySelector(".noteList")
@@ -19,24 +21,46 @@ else {
 
 })
 eventHub.addEventListener("noteStateChanged", () => {
-    if (visible === true) {
+    // if (visible === true) {
     NoteList()
-    }
+    // }
 })
+const render = (noteArray, criminals) => {
+   
+        // Find the related criminal
+        
+        const allNotesConvertedToStrings = noteArray.map( (note) => {
+            const relatedCriminal = criminals.find(
+                (criminal) => {
+               return criminal.id === note.criminalId
+            }
+            )
+                 note.criminalName = relatedCriminal.name
 
-const render = (noteArray) => {
-    const allNotesConvertedToStrings = noteArray.map( (note) => NoteHTMLConverter (note) ).join("")
+        
+        return  NoteHTMLConverter (note)
+         } ).join("")
     
         // convert the notes objects to HTML with NoteHTMLConverter
 
     contentTarget.innerHTML = allNotesConvertedToStrings
-}
+        }
+
+//         return `
+//             <section class="note">
+//                 <h2>Note about ${relatedCriminal.name}</h2>
+//                 ${note.noteText}
+//             </section>
+//         `
+//     })
+// }
 
 // Standard list function you're used to writing by now. BUT, don't call this in main.js! Why not?
 export const NoteList = () => {
+    let criminals = useCriminals()
     getNotes()
         .then(() => {
             const allNotes = useNotes()
-            render(allNotes)
+            render(allNotes,criminals)
         })
 }
