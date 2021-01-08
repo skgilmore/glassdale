32444b14b03ecf087f480ea8d1c34a625d67b42f
skgilmore <skg.gilmore@gmail.com>
2021-01-08 09:09:00 -0600

    added facilities to criminals

--- scripts/criminals/criminals.js
+++ scripts/criminals/criminals.js
@@ -1,4 +1,4 @@
-export const criminals = (criminalsObject) => {
+export const criminals = (criminalsObject, facilities) => {
     return `
     <section class="criminals data">
     <h2 class="criminals__name">${criminalsObject.name}</h2>
@@ -6,10 +6,17 @@ export const criminals = (criminalsObject) => {
     <div class="criminals__conviction">Crime: ${criminalsObject.conviction}</div>
     <div class="criminals__incarcerationStart">Incarcerated" ${new Date(criminalsObject.incarceration.start).toLocaleDateString('en-US')}</div>
     <div class="criminals__incarcerationEnd">Released: ${new Date(criminalsObject.incarceration.end).toLocaleDateString('en-US')}</div>
+    <div>
+    <h2>Facilities</h2>
+    <ul>
+        ${facilities.map(forEachObject => `<li>${forEachObject.facilityName}</li>`).join("")}
+    </ul>
+</div>
     </section>
     
     `
 }
 
+    // <button id="associates--${criminal.id}">Associate Alibis</button>
 
 
